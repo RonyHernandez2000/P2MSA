214dd57a7c98709d10adee39d038d9d5f8118b0a
Ben Franco <88692753+BenFranco47@users.noreply.github.com>
2022-03-12 10:11:45 -0500

    Update

--- src/App.js
+++ src/App.js
@@ -1,33 +1,14 @@
-<<<<<<< HEAD
-
-/*import Navbar from "./components/Navbar"
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
-=======
-
 import './App.css';
-import LoginLogic from './components/loginLogic';
->>>>>>> 38bdb9b9766a84ae02ef1729e8ce44a1c7cabaac
-*/
-
+import Main from './components/Main';
 
 function App() {
  
   return (
-<<<<<<< HEAD
-    
-  /*    <Router>
-        <Navbar />
-        <Switch>
-          <Route path="/" />
-        </Switch>
-      </Router>
-    */
-=======
    <div className="app">
-     <LoginLogic/>
-    </div> 
->>>>>>> 38bdb9b9766a84ae02ef1729e8ce44a1c7cabaac
+     <Main/>
+    </div>
   );
 }
 
 export default App;
+
