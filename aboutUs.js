0a82803fab8f4e38c8bd5dfeb094e2883b62062f
RonyHernandez2000 <89493923+RonyHernandez2000@users.noreply.github.com>
2022-03-15 18:52:53 -0400

    updated about us file

---
+++ aboutUs.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+function AboutUS(){
+return(
+    <div>
+    <h2>Front End Dev</h2>
+    <br></br>
+    <h3>Frank Bourdier</h3>
+    <br></br>
+    <h2>Back End Dev</h2>
+    <br></br>
+    <h3>Benjamin Franco</h3>
+    <h3>Rony Hernandez</h3>
+    <br></br>
+    <h2>Why did we make this app?</h2>
+    <p>We as developers made an idea of having a social media app that would be like peer to peer/ hearsay that is the reason for the code before getting 
+        to interact with the app. Then after we loved the idea of having random anonymous account given to you and you are given categories to choose from after that you click of said 
+        categories that would give you current events, and in that you can have discussion at that time period with somebody else, We love the idea of bringing random people together give each other there takes on current events they are interested in.</p>
+    </div>
+)
+
+}
+
+export default AboutUS
