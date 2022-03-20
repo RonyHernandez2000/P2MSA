648e7ed4cbb0aade28577bf307aa48cc3ce39fef
Frank Bourdier <67430411+LOGICAL98@users.noreply.github.com>
2022-03-19 22:25:16 -0400

    Update home.js

--- src/components/home.js
+++ src/components/home.js
@@ -1,11 +1,15 @@
-import React from 'react'
+import React from "react";
+function Home(){
+return (
+   <nav>
+  <ul class ="nav">
+  <li><a href="" img src ="src/components/Entertainment.jpg" class="active">Home</a></li>
+  <li><a href="entertainment">Entertainment</a></li>
+  <li><a href="news">News</a></li>
+  <li><a href="sports">Sports</a></li>
+  <li><a href="devs">Devs</a></li>
+  </ul>
+  </nav>
+)} 
 
-function Home() {
-    return(
-        <h1>Home Page</h1>
-    )
-
-    
-
-}
-export default Home
+export  default Home
